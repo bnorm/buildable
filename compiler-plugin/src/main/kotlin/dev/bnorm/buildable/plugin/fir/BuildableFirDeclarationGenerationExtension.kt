eba3118a68640c36f64f011a45bfccdc4a67262d
Brian Norman <brian.norman@jetbrains.com>
2025-04-06 22:54:52 -0500

    Inline FIR factory functions

--- compiler-plugin/src/main/kotlin/dev/bnorm/buildable/plugin/fir/BuildableFirDeclarationGenerationExtension.kt
+++ compiler-plugin/src/main/kotlin/dev/bnorm/buildable/plugin/fir/BuildableFirDeclarationGenerationExtension.kt
@@ -1,14 +1,19 @@
-package dev.bnorm.buildable.plugin.fir.declarations
+package dev.bnorm.buildable.plugin.fir
 
 import dev.bnorm.buildable.plugin.BuildableKey
 import dev.bnorm.buildable.plugin.BuildableNames
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.analysis.checkers.getContainingClassSymbol
+import org.jetbrains.kotlin.fir.declarations.utils.visibility
 import org.jetbrains.kotlin.fir.extensions.*
 import org.jetbrains.kotlin.fir.extensions.predicate.LookupPredicate
-import org.jetbrains.kotlin.fir.extensions.predicate.LookupPredicate.BuilderContext.annotated
 import org.jetbrains.kotlin.fir.plugin.createConstructor
+import org.jetbrains.kotlin.fir.plugin.createMemberFunction
+import org.jetbrains.kotlin.fir.plugin.createMemberProperty
+import org.jetbrains.kotlin.fir.plugin.createNestedClass
+import org.jetbrains.kotlin.fir.resolve.providers.getRegularClassSymbolByClassId
 import org.jetbrains.kotlin.fir.symbols.impl.*
+import org.jetbrains.kotlin.fir.types.constructType
 import org.jetbrains.kotlin.name.CallableId
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.Name
@@ -21,9 +26,15 @@ class BuildableFirDeclarationGenerationExtension(
   companion object {
     //@sample-start:ANNOTATION_PREDICATE
     private val ANNOTATION_PREDICATE: LookupPredicate =
-      annotated(BuildableNames.BUILDABLE_FQ_NAME)
+      LookupPredicate.BuilderContext.annotated(BuildableNames.BUILDABLE_FQ_NAME)
     //@sample-end
   }
+
+  //@sample-start:registerPredicates
+  override fun FirDeclarationPredicateRegistrar.registerPredicates() {
+    register(ANNOTATION_PREDICATE)
+  }
+  //@sample-end
 
   //@sample-start:classIds
   private val classIds: Map<ClassId, FirConstructorSymbol> by lazy {
@@ -45,12 +56,6 @@ class BuildableFirDeclarationGenerationExtension(
     classIds.mapKeys { (key, _) ->
       key.createNestedClassId(BuildableNames.BUILDER_CLASS_NAME)
     }
-  }
-  //@sample-end
-
-  //@sample-start:registerPredicates
-  override fun FirDeclarationPredicateRegistrar.registerPredicates() {
-    register(ANNOTATION_PREDICATE)
   }
   //@sample-end
 
@@ -76,7 +81,16 @@ class BuildableFirDeclarationGenerationExtension(
     if (name != BuildableNames.BUILDER_CLASS_NAME) return null
     val constructorSymbol = classIds[owner.classId] ?: return null
 
-    return createBuilderClass(owner, constructorSymbol).symbol
+    val builderClass = createNestedClass(
+      owner = owner,
+      name = BuildableNames.BUILDER_CLASS_NAME,
+      key = BuildableKey
+    ) {
+      visibility = constructorSymbol.originalVisibility
+        ?: constructorSymbol.visibility
+    }
+
+    return builderClass.symbol
   }
   //@sample-end
 
@@ -100,12 +114,12 @@ class BuildableFirDeclarationGenerationExtension(
   override fun generateConstructors(
     context: MemberGenerationContext,
   ): List<FirConstructorSymbol> {
-    val classSymbol = context.owner
-    if (classSymbol.classId !in builderClassIds) return emptyList()
+    val builderSymbol = context.owner
+    if (builderSymbol.classId !in builderClassIds) return emptyList()
 
-    return listOf(
-      createConstructor(classSymbol, BuildableKey, isPrimary = true).symbol
-    )
+    val constructor =
+      createConstructor(builderSymbol, BuildableKey, isPrimary = true)
+    return listOf(constructor.symbol)
   }
   //@sample-end
 
@@ -122,9 +136,16 @@ class BuildableFirDeclarationGenerationExtension(
       .singleOrNull { it.name == callableId.callableName }
       ?: return emptyList()
 
-    return listOf(
-      createBuilderProperty(classSymbol, parameterSymbol).symbol
+    val property = createMemberProperty(
+      owner = classSymbol,
+      key = BuildableKey,
+      name = parameterSymbol.name,
+      returnType = parameterSymbol.resolvedReturnType,
+      isVal = false,
+      hasBackingField = false,
     )
+
+    return listOf(property.symbol)
   }
   //@sample-end
 
@@ -136,12 +157,20 @@ class BuildableFirDeclarationGenerationExtension(
     if (callableId.callableName != BuildableNames.BUILD_FUN_NAME)
       return emptyList()
 
-    val classSymbol = context?.owner ?: return emptyList()
-    if (classSymbol.classId !in builderClassIds) return emptyList()
+    val builderSymbol = context?.owner ?: return emptyList()
+    if (builderSymbol.classId !in builderClassIds) return emptyList()
 
-    return listOf(
-      createBuilderBuild(classSymbol, callableId).symbol
+    val buildableClassId = builderSymbol.classId.outerClassId!!
+    val buildableSymbol =
+      session.getRegularClassSymbolByClassId(buildableClassId)!!
+    val build = createMemberFunction(
+      owner = builderSymbol,
+      key = BuildableKey,
+      name = callableId.callableName,
+      returnType = buildableSymbol.constructType(),
     )
+
+    return listOf(build.symbol)
   }
   //@sample-end
 }
--- compiler-plugin/src/main/kotlin/dev/bnorm/buildable/plugin/fir/BuildableFirExtensionRegistrar.kt
+++ compiler-plugin/src/main/kotlin/dev/bnorm/buildable/plugin/fir/BuildableFirExtensionRegistrar.kt
@@ -1,7 +1,6 @@
 package dev.bnorm.buildable.plugin.fir
 
 import dev.bnorm.buildable.plugin.fir.checkers.BuildableFirAdditionalCheckersExtension
-import dev.bnorm.buildable.plugin.fir.declarations.BuildableFirDeclarationGenerationExtension
 import org.jetbrains.kotlin.fir.extensions.FirExtensionRegistrar
 
 //@sample-start:BuildableFirExtensionRegistrar
